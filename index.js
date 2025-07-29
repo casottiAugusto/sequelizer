41bde4f41b624c0564774c1913ed39ec3c145d65
Joao Casotti <agusto_casotti@hotmail.com>
2025-07-29 20:20:52 -0300

    Selecionando todos e escundo um registro

--- index.js
+++ index.js
@@ -30,12 +30,27 @@ app.post('/users/create', async (req, res) => {
    res.redirect('/');
 
 })
-
+app.post('/users/delete/:id', async (req, res) => {
+    const id = req.params.id;   
+    await User.destroy({where: {id: id}});
+    res.redirect('/');
+});
 app.get('/',async (req, res) => {
     const users=await User.findAll({raw: true});
-    console.log(users);
+    
     res.render('home',{users:users});
 });
+
+app.get('/users/:id', async (req, res) => {
+     const id = req.params.id;
+  const user = await User.findOne({ raw: true, where: { id: id } });
+ 
+ 
+  res.render('userview',{user});
+});
+
+
+
 conn.sync()
     .then(() => {
         console.log('Conexão com o banco de dados estabelecida com sucesso.');
